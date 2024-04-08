bc76b13d39f62dba85f999cc9cc54552f33b4fa5
Humayun Habib <humayunhabib786@gmail.com>
2024-04-08 14:27:58 +0500

    open button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { WagmiProvider } from "wagmi";
+import { arbitrum, mainnet } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "9df0990f61563b811deba890efbcd83f";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum];
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <App />
+    </WagmiProvider>
   </React.StrictMode>
 );
 
